8a95dcbe71426cd11b199ad61cb4547a8d60a9d6
lifengli13 <lilifeng1984@gmail.com>
2019-07-09 09:13:48 -0700

    Update ip_mac.sh

--- ip_mac.sh
+++ ip_mac.sh
@@ -1,1 +1,1 @@
-ip=`ifconfig enp6s0 | grep -o 'inet [0-9.]*' | awk '{print $2}'`;mac=`ifconfig enp6s0 | grep -o 'ether [0-9a-f:]*'  | awk '{print $2}'`;echo "`hostname`    $ip $mac"
+ip=`ifconfig enp6s0 | grep -o 'inet [0-9.]*' | awk '{print $2}'`;mac=`ifconfig enp6s0 | grep -o 'ether [0-9a-f:]*'  | awk '{print $2}'`;echo "`hostname`-enp6s0 $ip $mac"
